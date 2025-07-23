44e3588a37de4d6083aed00f2a6e69480b4a26f2
shelmith <shelwangui23@gmail.com>
2025-07-23 09:39:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,29 @@ import BookstorePanelAdmin from "./BookstorePanelAdmin";
 
 function App() {
   const [books, setBooks] = useState([]);
-  
 
-  useEffect(() => {fetch("http://localhost:6001/books")
-    .then((r) => r.json())
-    .then((data) => setBooks(data));
-  },[]);
+  useEffect(() => {
+    fetch("http://localhost:6001/books")
+      .then((r) => {
+        if (!r.ok) {
+          throw new Error("Failed to fetch books");
+        }
+        return r.json();
+      })
+      .then((data) => setBooks(data))
+      .catch((error) => console.error("Fetch error:", error));
+  }, []);
 
   function handleAddBook(newBook) {
     setBooks([...books, newBook]);
   }
+
   return (
     <div className="App">
-      <BookstorePanelAdmin books = {books}/>
+      <BookstorePanelAdmin books={books} />
     </div>
-
-    
   );
 }
 
 export default App;
+
